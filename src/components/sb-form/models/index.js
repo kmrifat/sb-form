f83cffebfdbfe412dda84f4a900f45f81ae85b43
Ekramul Islam Sumon <38817030+sumoninfo@users.noreply.github.com>
2023-06-19 21:49:33 +0600

    multi select file name updated

--- src/components/sb-form/models/index.js
+++ src/components/sb-form/models/index.js
@@ -6,7 +6,7 @@ import SelectField from "./SelectField";
 import HiddenField from "./HiddenField";
 import TextField from "./TextField";
 import DateField from "./DateField";
-import MultiSelectField from "./MultiSelectField";
+import MultiselectField from "./MultiselectField";
 import CheckboxField from "./CheckboxField";
 import {CustomField, VueComponentProp} from "./CustomField";
 
@@ -16,7 +16,7 @@ export {
     EmailField,
     FileFiled,
     SelectField,
-    MultiSelectField,
+    MultiselectField,
     HiddenField,
     TextField,
     DateField,
