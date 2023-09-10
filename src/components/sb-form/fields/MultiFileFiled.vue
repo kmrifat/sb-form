f61e51fc38230e8a6501eae47740eff75eaa1f69
coderasad <39905304+coderasad@users.noreply.github.com>
2023-09-10 23:20:46 +0600

    Multi photo input design issue fixed

--- src/components/sb-form/fields/MultiFileFiled.vue
+++ src/components/sb-form/fields/MultiFileFiled.vue
@@ -1,21 +1,24 @@
 <template>
-  <div class="col-12 file-filed">
+  <div class="col-12 file-filed mb-3 position-relative">
     <label class="form-label mt-3 mb-0">{{ fieldInfo.label }} {{ fieldInfo.required ? '*' : '' }}</label>
-    <div class="card shadow-none mt-3">
-      <div class="card-body p-1" v-if="selectedFiles.length">
-        <div class="image-container">
-          <div v-for="(file, index) in selectedFiles" :key="index" class="image-item border">
-            <div v-if="isImageFile(file.type)" class="preview" :style="'background-image:url('+file.path+')'"></div>
+    <div class="card shadow-none mt-3 border-0 flex-row gap-3">
+      <div class="card-body p-0">
+        <div class="image-container d-flex flex-wrap align-items-center gap-3">
+          <div v-for="(file, index) in selectedFiles" :key="index" class="position-relative">
+            <div v-if="isImageFile(file.type)" class="preview w-192 img-thumbnail" :style="'background-image:url('+file.path+')'"></div>
             <h3 v-else class="file-extension my-auto">{{ getFileExtension(file.path) }}</h3>
-            <button type="button" @click="removeSelectedFile(index)" class="remove-btn btn btn-danger btn-sm rounded-0">Remove</button>
+            <button type="button" @click="removeSelectedFile(index)" class="remove-btn btn btn-danger btn-sm rounded-circle position-absolute">
+              &#128473;
+            </button>
+          </div>
+          <div class="align-items-center border card-body d-flex flex-grow-0 flex-shrink-0 justify-content-center rounded text-center w-192" type="button" @click="toggleModal">
+            <i class="fas fa-cloud-upload fa-6x"></i>
+            <p class="card-text">Click to upload or select files from File Manager.</p>
           </div>
         </div>
       </div>
 
-      <div class="card-body" @click="toggleModal">
-        <i class="fas fa-cloud-upload fa-6x"></i>
-        <p class="card-text">Click to upload or select files from File Manager.</p>
-      </div>
+
     </div>
     <div :class="fieldInfo.error ? 'is-invalid': ''"></div>
     <div class="invalid-feedback">
@@ -185,45 +188,21 @@ export default {
 
 .file-filed {
   .card {
-    width: 100%;
-    height: auto;
-
     .card-body {
-      .image-container {
-        display: flex;
-        flex-wrap: wrap;
-        gap: 10px;
-      }
-
-      .image-item {
-        position: relative;
-        width: 100px; /* Adjust width as needed */
-        height: 100px; /* Adjust height as needed */
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-      }
-
       .preview {
-        height: 80px;
-        width: 100%;
         background-repeat: no-repeat;
         background-origin: inherit;
         background-size: cover;
       }
-
       .remove-btn {
-        margin-top: 5px;
+        top: -8px;
+        right: -8px;
       }
     }
-
     .card-text {
       font-size: 10px;
     }
   }
-
-  margin-bottom: 15px;
-  position: relative;
 }
 
 
@@ -261,5 +240,9 @@ export default {
   }
 }
 
+.w-192{
+  width : 192px;
+  height : 192px;
+}
 
 </style>
